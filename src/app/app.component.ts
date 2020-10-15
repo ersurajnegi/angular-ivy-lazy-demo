a04c0bc0a7bb4cc5669b9153bf5c780f364da832
ersurajnegi <er.surajnegi@gmail.com>
2020-10-15 19:31:16 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,9 @@ import {
   ViewContainerRef
 } from "@angular/core";
 
-import { LazyComponent } from "./lazy/lazy.component";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  name = "Angular " + VERSION.major;
-  @ViewChild(TemplateRef, { read: ViewContainerRef })
-  private templateViewContainerRef: ViewContainerRef;
-
-  constructor(
-    private readonly componentFactoryResolver: ComponentFactoryResolver
-  ) {}
-
-  load() {
-    this.templateViewContainerRef.clear();
-    import("./lazy/lazy.component").then(({ LazyComponent }) => {
-      const component = this.componentFactoryResolver.resolveComponentFactory(
-        LazyComponent
-      );
-      const componentRef = this.templateViewContainerRef.createComponent(
-        component
-      );
-      componentRef.instance.test = "Dynamically loaded"
-    });
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
-import { LazyComponent } from "./lazy/lazy.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, LazyComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
